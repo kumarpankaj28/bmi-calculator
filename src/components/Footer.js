416236fdebdc92c6efe3d09123e4f49353ca0705
Pankaj Kumar <pankaj3359kr@gmail.com>
2022-02-21 02:37:43 +0530

    Add logic and style the app

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div class="footer">Built by <a href="https://github.com/ipankajkr" target="_blank" className="strong">Pankaj Kumar</a>.</div>
+  )
+}
+
+export default Footer
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className="header shadow-md">
+        <a href="./"><h1 className="font-bold leading-tight text-3xl">BMI <span className="focused-text">Calculator</span></h1>
+        </a>
+    </div>
+  )
+}
+
+export default Header
